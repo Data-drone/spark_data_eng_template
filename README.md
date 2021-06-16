6c2f1b6f8ca689149fa05929d0b68e5088c4e7cd
Data-drone <bpl.law@gmail.com>
2021-06-16 14:02:29 +1000

    added a thrift server for odbc/jdbc

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # spark_data_eng_template
 The datalake portion of my opensource ml platform
+
+
+## Todo
+
+Get the Spark Thrift Server started for odbc connections integrated as part of it
+
---
+++ spark_thrift_server/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/spark/bin/spark-submit --master ${SPARK_MASTER} \
+    --class org.apache.spark.sql.hive.thriftserver.HiveThriftServer2 \
+    --conf spark.hadoop.fs.s3a.access.key=${MINIO_ACCESS_KEY} \
+    --conf spark.hadoop.fs.s3a.secret.key=${MINIO_SECRET_KEY} \
+    --conf spark.hadoop.fs.s3a.endpoint=minio:9000 \
+    --conf spark.hadoop.fs.s3a.connection.ssl.enabled=false \
+    --conf spark.hadoop.fs.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
+    --conf spark.sql.warehouse.dir=s3a://storage/warehouse \
+    --conf spark.hadoop.fs.s3a.path.style.access=true \
+    --conf spark.hive.metastore.uris=thrift://172.30.0.4:9083 \
+    /
